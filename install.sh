3f5a8ae38954b9de0f9115ba93536d24a60326fa
bmaupin <bmaupin@users.noreply.github.com>
2024-02-26 20:14:57 -0500

    fix: Mod files and directories have to be lower-case
    ... in Linux. Also add updated checksums. They don't seem to be case-sensitive.

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 mod_name="Russian invasion of Ukraine (v 1)"
 
-# user_directory="/home/${USER}/.steam/steam/steamapps/compatdata/8930/pfx/drive_c/users/steamuser/Documents/My Games/Sid Meier's Civilization 5/"
-user_directory="/home/${USER}/.local/share/Aspyr/Sid Meier's Civilization 5/"
+# user_directory="/home/${USER}/.steam/steam/steamapps/compatdata/8930/pfx/drive_c/users/steamuser/Documents/My Games/Sid Meier's Civilization 5"
+user_directory="/home/${USER}/.local/share/Aspyr/Sid Meier's Civilization 5"
 
 # Update mod file checksums
 pushd src > /dev/null
@@ -17,11 +17,15 @@ done
 popd > /dev/null
 
 maps_directory="${user_directory}/Maps"
-mod_directory="${user_directory}/MODS/${mod_name}"
+mod_directory="${user_directory}/MODS/$(echo "${mod_name}" | tr '[:upper:]' '[:lower:]')"
 
 cp src/Ukraine_29x29.Civ5Map "$maps_directory"
 
+# We have to clean up the mod first because otherwise rename will fail because the files will exist
+rm -rf "${mod_directory}"
 mkdir -p "$mod_directory"
 cp -r src/* "$mod_directory"
 rm "${mod_directory}/Ukraine_29x29.Civ5Map"
 cp "${mod_name}.modinfo" "$mod_directory"
+# All files have to be renamed to lower-case in Linux for it to work (https://stackoverflow.com/a/152741)
+find "${mod_directory}" -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
