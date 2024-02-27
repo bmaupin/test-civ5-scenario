d8be088339fb74ffa1e835c9aee1d4d2cba3d0df
bmaupin <bmaupin@users.noreply.github.com>
2024-02-26 20:30:06 -0500

    feat: Move .modinfo file to src/

--- install.sh
+++ install.sh
@@ -7,13 +7,17 @@ user_directory="/home/${USER}/.local/share/Aspyr/Sid Meier's Civilization 5"
 
 # Update mod file checksums
 pushd src > /dev/null
+# Override IFS (internal field separator) in order to handle files with spaces in name
+original_IFS="$IFS"
+IFS=$'\n'
 for filename in $(find . -type f | cut -c 3-); do
     new_md5sum=$(md5sum "$filename" | awk '{print $1}')
-    old_md5sum=$(grep "$filename" ../"${mod_name}.modinfo" | awk '{print $2}' | cut -c 6- | rev | cut -c 2- | rev)
+    old_md5sum=$(grep "$filename" "${mod_name}.modinfo" | awk '{print $2}' | cut -c 6- | rev | cut -c 2- | rev)
     if [[ -n $old_md5sum ]]; then
-        sed -i "s/${old_md5sum}/${new_md5sum}/" ../"${mod_name}.modinfo"
+        sed -i "s/${old_md5sum}/${new_md5sum}/" "${mod_name}.modinfo"
     fi
 done
+IFS="$original_IFS"
 popd > /dev/null
 
 maps_directory="${user_directory}/Maps"
@@ -26,6 +30,5 @@ rm -rf "${mod_directory}"
 mkdir -p "$mod_directory"
 cp -r src/* "$mod_directory"
 rm "${mod_directory}/Ukraine_29x29.Civ5Map"
-cp "${mod_name}.modinfo" "$mod_directory"
 # All files have to be renamed to lower-case in Linux for it to work (https://stackoverflow.com/a/152741)
 find "${mod_directory}" -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
