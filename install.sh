92d13cd5bdcbe9b2323b981a31e5c49feb135df3
bmaupin <bmaupin@users.noreply.github.com>
2024-02-28 20:38:11 -0500

    refactor: Move XML and maps into subdirectories
    This seems to be common with other mods and should be nicer as we add more files

--- install.sh
+++ install.sh
@@ -24,12 +24,14 @@ popd > /dev/null
 maps_directory="${user_directory}/Maps"
 mod_directory="${user_directory}/MODS/$(echo "${mod_name}" | tr '[:upper:]' '[:lower:]')"
 
-cp src/Ukraine_29x29.Civ5Map "$maps_directory"
+# TODO: remove this once we make the map part of the mod
+cp src/Maps/Ukraine_29x29.Civ5Map "$maps_directory"
 
 # We have to clean up the mod first because otherwise rename will fail because the files will exist
 rm -rf "${mod_directory}"/*
 mkdir -p "$mod_directory"
 cp -r src/* "$mod_directory"
-rm "${mod_directory}/Ukraine_29x29.Civ5Map"
+# TODO: remove this once we make the map part of the mod
+rm "${mod_directory}/Maps/Ukraine_29x29.Civ5Map"
 # All files have to be renamed to lower-case in Linux for it to work (https://stackoverflow.com/a/152741)
 find "${mod_directory}" -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
